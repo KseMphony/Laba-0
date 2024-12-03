11441b10408cc52942fbbbaaad1cb23368d746ba
KseMphony <xenia.bursak@yandex.com>
2024-12-03 09:56:48 +0300

    код

---
+++ laba0bursakcode.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char userSymbol; 
+    string greeting = "Hello World!!!"; 
+
+    cout << greeting << endl; 
+    cout << "Please input a character: ";
+    cin >> userSymbol; 
+    cout << "You entered: " << userSymbol << endl;
+}
